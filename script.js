2f4f199f4ab2ec495125e2bc4715950296d8582a
Lanmijami <milanvukasinovic5@gmail.com>
2026-03-24 20:30:56 +0100

    Color changing logic added

--- script.js
+++ script.js
@@ -14,15 +14,22 @@ let num = 0;
 const img = document.querySelector('.img-container .car-img');
 const spanOne = document.querySelectorAll('.span-container span')[0];
 const spanTwo = document.querySelectorAll('.span-container span')[1];
+const body = document.querySelector('body');
 
 if (days % 2 == 0) {
   img.src = 'Assets/RenoMojGotov.png';
   spanOne.innerText = 'D';
   spanTwo.innerText = 'A';
+  body.style.backgroundColor = 'rgb(4, 47, 14)';
+  spanOne.style.color = 'rgb(12, 75, 30)';
+  spanTwo.style.color = 'rgb(12, 75, 30)';
 } else {
   img.src = 'Assets/FireCarThreGotovo.png';
   spanOne.innerText = 'N';
   spanTwo.innerText = 'E';
+  body.style.backgroundColor = 'rgb(40, 14, 18)';
+  spanOne.style.color = 'rgb(62, 32, 37)';
+  spanTwo.style.color = 'rgb(62, 32, 37)';
 }
 
 setInterval(() => {
