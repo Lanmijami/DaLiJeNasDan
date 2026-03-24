bba2d924bce576073ffc2d6d5c64cb4a1c82f9a5
Lanmijami <milanvukasinovic5@gmail.com>
2026-03-24 20:22:28 +0100

    Added the YES/NO logic

--- script.js
+++ script.js
@@ -12,10 +12,18 @@ const days = Temporal.PlainDate.from(start).until(
 
 let num = 0;
 const img = document.querySelector('.img-container .car-img');
+const spanOne = document.querySelectorAll('.span-container span')[0];
+const spanTwo = document.querySelectorAll('.span-container span')[1];
 
 if (days % 2 == 0) {
   img.src = 'Assets/RenoMojGotov.png';
-} else img.src = 'Assets/FireCarThreGotovo.png';
+  spanOne.innerText = 'D';
+  spanTwo.innerText = 'A';
+} else {
+  img.src = 'Assets/FireCarThreGotovo.png';
+  spanOne.innerText = 'N';
+  spanTwo.innerText = 'E';
+}
 
 setInterval(() => {
   if (days % 2 == 0) {
